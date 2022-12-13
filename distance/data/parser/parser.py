f804ed5414925309a951adc16de3d03e9c4f121e
johnmzz <john521116@hotmail.com>
2022-12-13 22:28:36 +1100

    refined granularity

--- distance/data/parser/parser.py
+++ distance/data/parser/parser.py
@@ -1,17 +1,43 @@
 import random
+import math
 
 def parse(name):
 
     f_in = open("../" + name + ".txt", 'r')
     f_out = open("../new.txt", 'w+')
 
+    edges = []
+
+    m = 0
+    t_min = math.inf
+    t_max = 0
+
     for line in f_in.readlines():
         lst = line.split()
 
         u = lst[0]
         v = lst[1]
-        t = lst[2]
+        t = int(lst[2])
 
-        f_out.write(u + " " + v + " " + t + " " + str(random.randint(1,100)) + "\n")
+        edge = [t, u, v]
+        edges.append(edge)
+
+        m += 1
+        if t > t_max:
+            t_max = t
+        if t < t_min:
+            t_min = t
+
+    edges.sort()
+    
+    new_t = 1
+    for i,e in enumerate(edges):
+        f_out.write(e[1] + " " + e[2] + " " + str(new_t) + " " + "1" + "\n")
+
+        if i % 100 == 0:
+            new_t += 1
+
+    print(f"m = {m}, t_max = {t_max}, t_min = {t_min}, num_t = {t_max-t_min}")
+    print(f"new_t = {new_t}")
 
 parse("CollegeMsg")
