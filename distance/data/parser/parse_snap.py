c09638dd03b94f27fbdfe00364aece4ba6a1fb21
johnmzz <john521116@hotmail.com>
2022-12-22 19:26:30 +1100

    data

--- distance/data/parser/parse_snap.py
+++ distance/data/parser/parse_snap.py
@@ -1,12 +1,15 @@
 import random
 import math
 
-def parse(name):
+def parse(name, data):
 
     f_in = open("../" + name + ".txt", 'r')
     f_out = open("../new.txt", 'w+')
 
     edges = []
+
+    t_min = math.inf
+    t_max = 0
 
     for line in f_in.readlines():
         lst = line.split()
@@ -14,17 +17,23 @@ def parse(name):
         u = lst[0]
         v = lst[1]
         t = int(lst[2])
+        t_min = min(t_min, t)
+        t_max = max(t_max, t)
 
         edge = [t, u, v]
         edges.append(edge)
 
     edges.sort()
+    print(f"t_max - t_min = {t_max - t_min}")
     
-    new_t = 1
     for i,e in enumerate(edges):
-        f_out.write(e[1] + " " + e[2] + " 1 " + str(new_t) + "\n")
+        f_out.write(e[1] + " " + e[2] + " 1 " + str((e[0] - t_min) // data[name] + 1) + "\n")
 
-        if i != 0 and i % 100 == 0:
-            new_t += 1
 
-parse("CollegeMsg")
+        
+
+data = {"CollegeMsg": 86715,
+        "email": 86499
+        }
+
+parse("CollegeMsg", data)
